dae66ce1a6fa0e08222eb2afbab1bf90a41bf98c
parisapourmand <parisa.pourmand@gmail.com>
2021-07-17 09:23:43 +0430

    errors resolved

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -3,8 +3,8 @@ from advertiser_management.models import BaseAdvertising, Advertiser, Ad
 
 advertiser1 = Advertiser.objects.create(name = "name1") 
 advertiser2 = Advertiser.objects.create(name = "name2")	
-ad1 = Ad.objects.create(title = "title1", imgURL = "img-url1", link = "link1", advetiser = advertiser1)
-ad2 = Ad.objects.create(title = "title2", imgURL = "img-url2", link = "link2", advetiser = advertiser2)
+ad1 = Ad.objects.create(title = "title1", imgURL = "img-url1", link = "link1", theAdvertiser = advertiser1)
+ad2 = Ad.objects.create(title = "title2", imgURL = "img-url2", link = "link2", theAdvertiser = advertiser2)
 advertisers = [advertiser1, advertiser2]
 
 def ads(request):
