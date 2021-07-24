8b49dac24f9a71ae8b118d1137bb7e5a828c9768
parisapourmand <parisa.pourmand@gmail.com>
2021-07-24 16:38:26 +0430

    postgresql database connected

--- Yektanet/settings.py
+++ Yektanet/settings.py
@@ -89,9 +89,17 @@ WSGI_APPLICATION = 'Yektanet.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'parisa',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
